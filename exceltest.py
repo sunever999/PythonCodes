1e70bad616966ab8b8b8c0fb28cfd8e0226acd68
sunever999 <sunever999@qq.com>
2013-04-08 11:45:28 +0800

    初步完成读取颜色数据并输出文件

--- exceltest.py
+++ exceltest.py
@@ -1,19 +1,84 @@
 import xlrd
 
-book = xlrd.open_workbook("myfile.xls")
-print "The number of worksheets is", book.nsheets
-print "Worksheet name(s):", book.sheet_names()
+def loopSheets():	
+	book = xlrd.open_workbook("/Users/Sam/Documents/myfile.xls")
+	#print "The number of worksheets is", book.nsheets
+	#print "Worksheet name(s):", book.sheet_names()
 
-for s in xrange(0, book.nsheets):
+	colorMatchDic = {}
 
-	table = book.sheets()[s]
-	nrows = table.nrows
-	ncols = table.ncols
+	for s in xrange(0, book.nsheets):
 
-	for x in xrange(0, ncols):
-		for y in xrange(1, nrows):
-			#colorInt = int(table.cell(y, x).value)
-			colorStr = str(table.cell(y, x).value).strip()
-			if colorStr != "":
-				print int(table.cell(y, x).value)
-		print "-----------------------------"
+		key = book.sheet_names()[s]
+
+		table = book.sheets()[s]
+		nrows = table.nrows
+		ncols = table.ncols
+
+		oneSheetList = []
+
+		for x in xrange(0, ncols):
+
+			oneColList = []
+			
+			for y in xrange(1, nrows):
+				#colorInt = int(table.cell(y, x).value)
+
+				colorStr = str(table.cell(y, x).value).strip()
+				if colorStr != "":
+					#print int(table.cell(y, x).value)
+					oneColList.append(int(table.cell(y, x).value))
+			#print "-----------------------------"
+			oneSheetList.append(oneColList)
+		#print (oneSheetList)
+
+		colorMatchDic[key] = oneSheetList
+
+	#print(colorMatchDic)
+	return colorMatchDic
+
+def writeFile(dataDic):
+	print "write file"
+
+	header = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>'''
+
+	end = '\n</dict>\n</plist>'
+
+	f = file('/Users/Sam/Documents/color_matches.plist', 'w')
+	f.write(header)
+
+	# out put data
+	for name, colorArray in dataDic.items():
+		#print 'Color:', name, ' data:', colorArray
+
+		theKey = '\n\t<key>'+name+'</key>'
+		f.write(theKey)
+
+		f.write('\n\t<array>')
+
+		for item in colorArray:
+			f.write('\n\t\t<array>')
+			for colorCode in item:
+				f.write('\n\t\t\t<string>'+str(colorCode)+'</string>')
+			f.write('\n\t\t</array>')
+
+		f.write('\n\t</array>')
+
+	f.write(end)
+	f.close()
+
+
+
+def main():
+	print "main here"
+
+	dic = loopSheets()
+	#print(dic)
+	writeFile(dic)
+
+if __name__=="__main__":
+	main()
